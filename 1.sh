5c76ea150d5046e02b1013c28385f59958675e6b
viveksinghrai2014 <mr.viviek@gmail.com>
2022-04-09 11:07:36 +0530

    Update and rename 1.sh to 3.sh

--- 1.sh
+++
@@ -1,1 +0,0 @@
-echo "bin/kafka-topics.sh --create --zookeeper ZookeeperConnectString --replication-factor 3 --partitions 1 --topic MSKTutorialTopic" > /tmp/kafka
---
+++ 3.sh
@@ -0,0 +1,3 @@
+echo "bin/kafka-topics.sh --create --zookeeper ZookeeperConnectString --replication-factor 3 --partitions 1 --topic MSKTutorialTopic" >> ~/kafka
+echo `date` >> ~/kafka
+
