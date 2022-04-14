36892a661199f163f348ef3c943235cd63dff25d
viveksinghrai2014 <mr.viviek@gmail.com>
2022-04-14 13:18:29 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,4 @@
-echo "bin/kafka-topics.sh --create --zookeeper ZookeeperConnectString --replication-factor 3 --partitions 1 --topic MSKTutorialTopic" >> ~/kafka
-echo `date` >> ~/kafka
+echo `date` >> /tmp/kafka
+echo "bin/kafka-topics.sh --create --zookeeper ZookeeperConnectString --replication-factor 3 --partitions 1 --topic MSKTutorialTopic" >> /tmp/kafka
+echo `date` >> /tmp/kafka
 
